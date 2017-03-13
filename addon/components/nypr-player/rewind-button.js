b10ebcdc76e7e5cf9c30bdb182e938cebc3e18c1
Brian Whitton <me@brianwhitton.com>
2017-03-12 22:44:13 -0400

    whitespace

--- addon/components/nypr-player/rewind-button.js
+++ addon/components/nypr-player/rewind-button.js
@@ -4,7 +4,7 @@ import layout from '../../templates/components/nypr-player/rewind-button';
 export default Ember.Component.extend({
   layout,
   tagName: 'button',
-  classNames:['nypr-player-button mod-rewind'],
+  classNames: ['nypr-player-button mod-rewind'],
   'aria-text': "rewind 15 seconds",
   'aria-labelled-by': "rewind-label"
 });
