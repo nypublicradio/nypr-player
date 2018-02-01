c90434df61cd6c5b99aa30bd14660be670d04db0
Brian Whitton <me@brianwhitton.com>
2018-02-01 00:16:41 -0500

    prep for deploying docs

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -44,7 +44,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    // here you can enable a production-specific feature
+    ENV.rootURL = '/nypr-player';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
