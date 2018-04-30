ab57a07308fbfdc40d8c407feb43e5a5fdb00bdb
Brian Whitton <me@brianwhitton.com>
2018-04-30 14:28:17 -0400

    add analytics bindings

--- addon/components/nypr-player/queue-button.js
+++ addon/components/nypr-player/queue-button.js
@@ -7,6 +7,7 @@ export default Component.extend({
   tagName           : 'button',
   classNames        : ['nypr-player-queue-button'],
   classNameBindings : ['isOpenModal:is-open', 'isFloating'],
+  attributeBindings : ['data-category', 'data-action', 'data-label', 'data-value'],
   queueLength       : null,
   showModal         : null,
   closeModal        : null,
