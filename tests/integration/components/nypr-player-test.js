b5ec7f2118de80f39b7e140f3f860637c16359cb
Brian Whitton <me@brianwhitton.com>
2018-04-30 10:07:50 -0400

    no longer passing in data-story or data-show

--- tests/integration/components/nypr-player-test.js
+++ tests/integration/components/nypr-player-test.js
@@ -6,16 +6,6 @@ moduleForComponent('nypr-player', 'Integration | Component | nypr player', {
   integration: true
 });
 
-test('can pass in story and show title to player', function(assert) {
-
-  this.render(hbs`
-    {{nypr-player storyTitle='story foo' showTitle='show bar'}}
-  `);
-
-  assert.equal(this.$('.nypr-player-button.mod-listen').attr('data-story'), 'story foo');
-  assert.equal(this.$('.nypr-player-button.mod-listen').attr('data-show'), 'show bar');
-});
-
 test('it renders', function(assert) {
   this.render(hbs`{{nypr-player}}`);
   assert.ok(find('.nypr-player'));
